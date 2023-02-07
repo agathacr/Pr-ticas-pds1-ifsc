eb59c9175af07b21f692951a80af08ce98efbfbf
Agatha <agatha.cr2004@aluno.isc.edu.br>
2023-02-07 08:56:32 -0300

    Pratica do dia 07/02/2023

---
+++ teste/src/controle/FuncionarioDAO.java
@@ -0,0 +1,19 @@
+package controle;
+
+import java.util.ArrayList;
+
+import modelo.Funcionario;
+
+public class FuncionarioDAO {
+	private static ArrayList<Funcionario> ListadeFuncionarios;
+	
+	public  FuncionarioDAO() {
+		ListadeFuncionarios = new ArrayList();
+	}
+	public boolean inserir (Funcionario func) {
+		ListadeFuncionarios.add(func);
+		return false;
+	}
+	}
+
+
